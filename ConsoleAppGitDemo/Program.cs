074046b943c1e7df239271ef0df357bb0b3e7f44
Nitin Roy <nitin98lko@gmail.com>
2021-06-12 15:06:21 +0530

    Add BranchBcmt

--- ConsoleAppGitDemo/Program.cs
+++ ConsoleAppGitDemo/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppGitDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello BranchB!");
         }
     }
 }
